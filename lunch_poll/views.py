a70d15768b9f6d3daf9c2cd389d3215fe99b9ac1
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-26 16:37:50 -0500

    setting up views and urls

--- lunch_poll/views.py
+++ lunch_poll/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, View
 
-# Create your views here.
+from .models import Lunch, Location
+
+
+class HomeView(TemplateView):
+
+    # view should be styled and have a listing of current lunches
+
+    template_name = 'lunch_poll/home.html'
+
+    pass
+
+
+class DetailView(TemplateView):
+    # view should show details of lunch,
+    # along with a list of locations
+
+    pass
+
+
+class ResultsView(TemplateView):
+
+    # view should show listing of locations for lunch
+    # along with number of votes each location received
+
+    pass
+
+
+class VoteView(TemplateView):
+
+    # view should recieve a POST request from the form on
+    # Detail page and increase vote count for selected location
+
+    pass
