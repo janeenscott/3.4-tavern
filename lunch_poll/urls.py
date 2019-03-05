70eb69cc2e7061e639c6a15c9b8d847e325bac49
Janeen <janeen.e.scott@gmail.com>
2019-03-05 16:49:11 -0500

    fixed url patterns to include primary key routes

--- lunch_poll/urls.py
+++ lunch_poll/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'lunch_poll'
+
 
 urlpatterns = [
 
     path('', views.HomeView.as_view(), name='home'),
-    path('details/', views.DetailView.as_view(), name='detail'),
-    path('results/', views.ResultsView.as_view(), name='results'),
-    path('vote/', views.VoteView.as_view(), name='vote'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:pk>/vote/', views.VoteView.as_view(), name='vote'),
 ]
