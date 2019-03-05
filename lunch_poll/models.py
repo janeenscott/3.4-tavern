b13d661de30b957cba17514b718fd0979181ae28
Janeen <janeen.e.scott@gmail.com>
2019-03-05 16:47:00 -0500

    added lunch views

--- lunch_poll/models.py
+++ lunch_poll/models.py
@@ -1,19 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Lunch(models.Model):
 
     nickname = models.CharField(max_length=50)
-    user = models.CharField(max_length=100)
-    date = models.DateField()
+    # user = models.CharField(max_length=100)
+    date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.nickname
 
-class Location(models.Model):
-    lunch = models.ForeignKey('lunch_poll.Lunch', on_delete=models.CASCADE)
-    votes = models.IntegerField()
 
+class Location(models.Model):
+    lunch = models.ForeignKey(Lunch, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, default='generic')
+    description = models.TextField(default=True)
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.lunch
+        return self.title
--- lunch_poll/views.py
+++ lunch_poll/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
+from django.utils import timezone
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from .models import Lunch, Location
 
@@ -8,18 +11,36 @@ class HomeView(TemplateView):
 
     # view should be styled and have a listing of current lunches
 
-    template_name = 'lunch_poll/home.html'
+    template_name = 'home.html'
 
-    pass
+    def get_context_data(self, **kwargs):
+        lunches = Lunch.objects.filter(
+            date__lte=timezone.now()
+        ).order_by('date')[:5]
+
+        context = {
+            'latest_lunches': lunches
+        }
+
+        return context
 
 
 class DetailView(TemplateView):
     # view should show details of lunch,
     # along with a list of locations
 
-    template_name = 'lunch_poll/detail.html'
+    template_name = 'detail.html'
 
-    pass
+    def get_context_data(self, **kwargs):
+        available_locations = Location.objects.all()
+        vote_pk = self.kwargs.get('pk')
+        vote = Lunch.objects.get(pk=vote_pk)
+        context = {
+            'details': available_locations,
+            'vote': vote,
+        }
+
+        return context
 
 
 class ResultsView(TemplateView):
@@ -27,7 +48,17 @@ class ResultsView(TemplateView):
     # view should show listing of locations for lunch
     # along with number of votes each location received
 
-    pass
+    template_name = 'results.html'
+
+    def get_context_data(self, **kwargs):
+
+        vote_pk = self.kwargs.get('pk')
+        vote = Lunch.objects.get(pk=vote_pk)
+        context = {
+            'vote': vote
+        }
+
+        return context
 
 
 class VoteView(View):
@@ -35,4 +66,16 @@ class VoteView(View):
     # view should recieve a POST request from the form on
     # Detail page and increase vote count for selected location
 
-    pass
+    def post(self, reqest, **kwargs):
+        vote_pk = self.kwargs.get('pk')
+        vote = Lunch.objects.get(pk=vote_pk)
+        lunch_spot_voted_id = self.request.POST.get('location')
+
+        selected_lunch_location = vote.location_set.get(pk=lunch_spot_voted_id)
+
+        selected_lunch_location.votes += 1
+        selected_lunch_location.save()
+
+        results_url = reverse('lunch_poll: results', args=(vote.pk,))
+
+        return HttpResponseRedirect(results_url)
